1e770c85f981701df069170e860dbba53ff76b77
Neel <neelpatel1337@gmail.com>
2023-06-27 18:55:59 -0400

    Cleaned up code, Added comments

--- server/lucki_ai.py
+++ server/lucki_ai.py
@@ -2,8 +2,11 @@ from llama_cpp import Llama
 import subprocess
 
 def water():
-    prompt = "Prompt: I am a bamboo plant named Lucky. You are my owner, Neel, a sillyy guy who has a big beard and uses Arch Linux. I am dying because you haven't watered you in multiple days. As a result, I, Lucky the bamboo, am writing a very long, mean, and insulting monologue. This is the monologue:"
+    # Preparing the prompt for the large language model
+    prompt = "Prompt: I am a bamboo plant named Lucky. You are my owner, Neel, a silly guy who has a big beard and uses Arch Linux. I am dying because you haven't watered you in multiple days. As a result, I, Lucky the bamboo, am writing a very long, mean, and insulting monologue. This is the monologue:"
     MAX_TOKENS = 400
+
+    # Output for the llm
     output = llm(prompt, max_tokens = MAX_TOKENS, stop=["Response:", "Neel:"])
 
     text = output["choices"][0]["text"]
@@ -12,14 +15,18 @@ def water():
     return text
 
 def convert_to_voice(text, filename):
+    # Use piper to convert to voice
+
     PIPER = '/home/neel/piper/piper'
     MODEL = '/home/neel/models/danny/en-us-danny-low.onnx'
 
+    # Subprocess to run bash commands in python
+    # although there are ways to interface piper in python directly
     echo_proc = subprocess.Popen(["echo", text], stdout=subprocess.PIPE)
     piper_proc = subprocess.Popen([PIPER, "--model", MODEL, "--output_file", filename], stdin=echo_proc.stdout)
     piper_proc.wait()
     piper_proc.communicate()
 
-
+# LLM Model is WizardLM 7b uncensored, so that it can make insults without any restrictions
 LLM_MODEL = '/home/neel/models/wizardlm-7b-uncensored-ggml-q4_0.bin'
 llm = Llama(model_path = LLM_MODEL)
--- server/server.py
+++ server/server.py
@@ -1,23 +1,28 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from llama_cpp import Llama
 from lucki_ai import water
 
 HOST = "10.0.0.61"
 PORT = 8000
 
 class RequestHandler(BaseHTTPRequestHandler):
+    # Get requests
     def do_GET(self):
+        # wget HOST:PORT/filename.wav -> downloads filename.wav
         if (".wav" in self.path):
             with open(self.path[1:], 'rb') as file:
                     self.send_response(200)
                     self.send_header('Content-type', 'audio/wav')
                     self.end_headers()
                     self.wfile.write(file.read())
+        
+        # curl HOST:PORT/*
         response = ""
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         
+        # curl HOST:PORT/WATER
+        # curl this URL if the plant needs water, output is the AI response
         if (self.path) == "/WATER":
             response = water()
 
